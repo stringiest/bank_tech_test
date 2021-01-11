90095882beda7920977df98f0466ee296de958ae
stringiest <lucyjstringer@gmail.com>
2021-01-11 14:20:38 +0000

    added deposit method & test to confirm that Account responds to deposit

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,10 @@
 class Account
   attr_reader :balance
-  
+
   def initialize(balance: 0)
     @balance = balance
+  end
+
+  def deposit(amount)
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,11 +1,19 @@
 require 'account'
 
 describe Account do
-  it 'should be an instance of Account' do
-    expect(subject).to be_an_instance_of Account
+  context 'new instances of Account' do
+    it 'should be an instance of Account' do
+      expect(subject).to be_an_instance_of Account
+    end
+
+    it 'should initialize with a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
   end
 
-  it 'should initialize with a balance of 0' do
-    expect(subject.balance).to eq 0
+  context 'depositing money' do
+    it 'responds to the method deposit' do
+      expect(subject).to respond_to(:deposit).with(1).arguments
+    end
   end
 end
